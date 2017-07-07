05110e5b866ea9f260526ed559d8c1ca04a45a61
shinmike <shinmike90@gmail.com>
2017-07-07 23:56:04 +0000

    add review model and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706220306) do
+ActiveRecord::Schema.define(version: 20170707234419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,17 @@ ActiveRecord::Schema.define(version: 20170706220306) do
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "product_id"
+    t.string   "user_id"
+    t.string   "description"
+    t.string   "rating"
+    t.string   "createdat"
+    t.string   "updatedat"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
