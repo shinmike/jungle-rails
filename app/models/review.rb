f042f56828147aea1d82f200aeb8e6bce277e548
shinmike <shinmike90@gmail.com>
2017-07-08 00:04:01 +0000

    add validation to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,14 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+
+  validate :rating_or_description
+
+  private
+
+  def rating_or_description
+    errors.add("Please rate or comment") if !rating && !description
+  end
 end
