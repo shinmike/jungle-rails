a16fcca1ecc8bfc1ee67e87560a6f1bda3ee64fc
shinmike <shinmike90@gmail.com>
2017-07-09 04:37:13 +0000

    step 8-12

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+  before_filter :authorize
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+
+    redirect_to :back if @review.save
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to :back
+  end
+  
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
+  end
 end
